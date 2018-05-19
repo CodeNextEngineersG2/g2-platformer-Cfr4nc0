a4c1ab1dea7b4b4291222e95fba119bff1dc5583
Student <student@Admins-MacBook-Air.local>
2018-05-19 11:50:13 -0700

    update

--- script.js
+++ script.js
@@ -180,20 +180,25 @@ function createCollectable(x, y) {
 // function calls executeLoss(). If a monster falls off the screen, it is
 // removed from the game.
 function applyGravity() {
-
+player.velocity.y += GRAVITY;
 }
 
 // Called in the draw() function. Continuously checks for collisions and overlaps
 // between all relevant game objects. Depending on the collision or overlap that
 // occurs, a specific callback function is run.
 function checkCollisions() {
-
+  player.collide(platforms, platformCollision);
 }
+//layer.collide(platform, platformCollision);
+
 
 // Callback function that runs when the player or a monster collides with a
 // platform.
 function platformCollision(sprite, platform) {
-
+if(sprite === player && sprite.touching.bottom){
+  sprite.velocity.y = 0;
+  playerGrounded = true;
+}
 }
 
 // Callback function that runs when the player collides with a monster.
@@ -209,9 +214,15 @@ function getCollectable(player, collectable) {
 // Updates the player's position and current animation by calling
 // all of the relevant "check" functions below.
 function updatePlayer() {
+  checkIdle();
+  checkFalling();
+  checkJumping();
+  checkMovingLeftRight();
+}
+
   //console.log("Player x: " + player.position.x + " Player y: " + player.position.y);
 
-}
+
 
 // Check if the player is idle. If neither left nor right are being pressed and the
 // player is grounded, set player's animation to "idle", and change her
@@ -238,7 +249,20 @@ function checkJumping() {
 // left or right according to DEFAULT_VELOCITY. Also be sure to mirror the
 // player's sprite left or right to avoid "moonwalking".
 function checkMovingLeftRight() {
-
+if(keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW)){
+  player.mirrorX(-1);
+  if(playerGrounded){
+    player.changeAnimation("run");
+     }
+    player.velocity.x = -DEFAULT_VELOCITY;
+  }
+else if(keyIsDown(RIGHT_ARROW)&& !keyIsDown(LEFT_ARROW)){
+  player.mirrorX(+1);
+  if(playerGrounded){
+    player.changeAnimation("run")
+  }
+  }
+  player.velocity.x = +DEFAULT_VELOCITY;
 }
 
 // Check if the player has pressed the up arrow key. If the player is grounded
